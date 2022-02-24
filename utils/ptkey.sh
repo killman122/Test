ba660f052911fab3d6f74d0c1d2787d3f04a737d
TongLin138 <bargain138@gmail.com>
2022-02-24 21:57:55 +0800

    Update

--- utils/ptkey.sh
+++ utils/ptkey.sh
@@ -42,7 +42,7 @@ fi
 cl(){
 echo "需要上传ptkey到青龙的请输入青龙常量，格式为
 ip:端口或青龙地址@client_id@client_secret
-例：ip:端口@VQjH8nYh_qSt@KRi575bgu7D-nNIZ5Kpv2O-"
+不需要的直接回车"
 stty -echo
 read -p "请输入青龙常量: " qlcl
 stty echo
@@ -117,8 +117,8 @@ err_msg=$(cat pt | grep -o "err_msg.*" | cut -d '"' -f3)
 if [ -z $err_msg ]
 then pt_key=$(cat pt | grep -o "pt_key.*" | cut -d '"' -f3)
 pt_pin=$(cat pt | grep -o "pt_pin.*" | cut -d '"' -f3)
-jdc="pt_key=$pt_key;pt_pin=$pt_pin;"
-if [ ! -z $tk ]
+[ ! -z "$pt_key" -a ! -z "$pt_pin" ] && jdc="pt_key=$pt_key;pt_pin=$pt_pin;" || jdc=""
+if [ ! -z "$tk" -a ! -z "$jdc" ]
 then echo jdc获取成功接下来为你上传至青龙
 ev
 if [ $(cat ev | grep -o "$pt_pin" | wc -l) -eq 0 ]
@@ -127,7 +127,9 @@ elif [ $(cat ev | grep -o "$pt_pin" | wc -l) -eq 1 ]
 then xg
 else echo 已存在多个相同jdc，请联系你的代挂删除后重新添加
 fi
-else echo 你的JD_COOKIE为 $jdc
+elif [ ! -z "$jdc" ]
+then echo 你的JD_COOKIE为 $jdc
+else echo "jdc获取失败，若重新执行脚本还是一样获取失败，请到电报联系 @shoujiyanxisheatu_bot"
 fi
 else echo $err_msg请检查手机号$mobile是否正确
 fi
